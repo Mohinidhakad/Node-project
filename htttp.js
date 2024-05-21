065668fa4f84b4a83df498ff82eeb307b92f93f6
Mohini Dhakad <99878435+Mohinidhakad@users.noreply.github.com>
2024-05-21 16:05:39 +0530

    Update htttp.js

--- htttp.js
+++ htttp.js
@@ -1,8 +1,22 @@
 const http =require('http')
 const server =http.createServer((request,response)=>
-    {
-
-        response.write("hello")
+    { 
+        const url =request.url;
+        switch(url)
+        {
+          case"/":
+            response.write("hello")
+            break
+          case "/About":
+            response.write("he")
+            break
+          case "/contact":
+            response.write("h")
+            break
+           case "/cart":
+            response.write("lo")
+            break
+        }
         response.end()
     })// server object with listning fun
     //start server
