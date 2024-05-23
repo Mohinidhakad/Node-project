da6700560d2961c83560a69dc227f3a8aebb1ae4
Mohini Dhakad <99878435+Mohinidhakad@users.noreply.github.com>
2024-05-23 13:43:06 +0530

    Update htttp.js
    http use to create server

--- htttp.js
+++ htttp.js
@@ -1,23 +1,48 @@
-const http =require('http')
+const http =require ('http')
 const server =http.createServer((request,response)=>
-    { 
+    {
         const url =request.url;
+        response.writeHead(200, { 'Content-Type': 'text/html' });// to recognize the text is html
+        const writeLinks = () => {
+          response.write("<hr>");
+          response.write("<a href='/'>Home</a>&nbsp;");
+          response.write("<a href='/about'>about</a>&nbsp;");
+          response.write("<a href='/contact'>contact</a>&nbsp;");
+          response.write("<a href='/cart'>cart</a>&nbsp;");
+      };
         switch(url)
         {
           case"/":
-            response.write("hello")
+            response.write("<h1>hello</h1>");
+            writeLinks();
+
             break
-          case "/About":
-            response.write("he")
+          case "/about":
+            response.write("welcome");
+            writeLinks();
+           
+           
             break
           case "/contact":
-            response.write("h")
+            response.write("come again");
+            writeLinks();
+            
+
             break
            case "/cart":
-            response.write("lo")
+            response.write("add");
+            writeLinks();
+            
+            
             break
         }
-        response.end()
+        response.end();
     })// server object with listning fun
+    
     //start server
-    server.listen(9898)
+    //server.listen(9898)
+    server.listen(9898, () => {
+      console.log('Server is listening on port 9898');
+  });
+           
+          
